935e5fdf8c72c5c7c838d5c2a5b609893f7891bb
TristanA0 <tristanlj@hotmail.fr>
2019-12-17 11:51:02 +0100

    Add soldByLocation model

---
+++ api/src/Entity/SoldByLocation.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\SoldByLocationRepository")
+ */
+class SoldByLocation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $solds;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getSolds(): ?int
+    {
+        return $this->solds;
+    }
+
+    public function setSolds(int $solds): self
+    {
+        $this->solds = $solds;
+
+        return $this;
+    }
+}
