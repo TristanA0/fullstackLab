e5072b9d6c56e3cad82760d11593d0a9f13bc926
Tristan Aubé <tristan.aube@etu.univ-lehavre.fr>
2020-01-03 21:57:23 +0100

    Add base for doctrine fixture bundle class

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\CountSold;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+
+use \DateTime;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        for ($i = 0; $i < 50; $i = $i+25) {
+            $countSold = new CountSold();
+            $countSold->setDate(new DateTime());
+            $countSold->setSolds($i);
+            $manager->persist($countSold);
+        }
 
         $manager->flush();
     }
