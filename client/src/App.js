0b2701cf79be4f798bb51dfa7091a97467c4279f
Julia Marinelli <julia_marinelli@hotmail.fr>
2020-01-05 21:54:52 +0100

    Add base front end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GraphTimeSeries from './GraphTimeSeries';
+
+class App extends React.Component {
+    componentDidMount() {
+      
+    }
+          
+    render(){
+      return (
+        <GraphTimeSeries />
+      )
+    }
+  }
+      
+  export default App;
---
+++ client/src/GraphTimeSeries.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+class GraphTimeSeries extends React.Component {
+    componentDidMount() {
+      this.draw();
+    }
+      
+    draw() {
+      
+    }
+          
+    render(){
+      return (
+        <div>
+        </div>
+      )
+    }
+  }
+      
+  export default GraphTimeSeries;
