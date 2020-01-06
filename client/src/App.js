08255a4502c97383e3b1f0e568090f53700dfa5e
julia marinelli <julia.marinelli@etu.univ-lehavre.fr>
2020-01-06 11:43:26 +0100

    fetch data for salesByRegion

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
         priceSquareMeter: [],
         isLoadedSquareMeter: false,
         salesByRegions: [],
+        isLoadedSalesByRegions: false,
         error: false
     }
 
@@ -14,18 +15,18 @@ class App extends React.Component {
         var month = '' + (d.getMonth() + 1),
             day = '' + d.getDate(),
             year = d.getFullYear();
-    
-        if (month.length < 2) 
+
+        if (month.length < 2)
             month = '0' + month;
-        if (day.length < 2) 
+        if (day.length < 2)
             day = '0' + day;
-    
+
         return [year, month, day].join('-');
     }
 
     componentDidMount() {
         const obj = this;
-        
+
         fetch('https://localhost:8443/property_value/price_square_meter_by_month')
         .then(res => res.json())
         .then((data) => {
@@ -35,24 +36,36 @@ class App extends React.Component {
                 let month = date.getMonth()+1;
                 let price = data["hydra:member"][i]["priceSquareM"];
 
-                if (!(date.getFullYear() in value)) 
+                if (!(date.getFullYear() in value))
                     value[date.getFullYear()] = [];
 
                 value[date.getFullYear()][month] = price;
             }
-            obj.setState({ priceSquareMeter: value, isLoadedSquareMeter: true, salesByRegions: [], error: false });
+            obj.setState({ priceSquareMeter: value, isLoadedSquareMeter: true, salesByRegions: [], isLoadedSalesByRegions : false, error: false });
         })
-        .catch(err => { obj.setState({ priceSquareMeter: [], salesByRegions: [], isLoadedSquareMeter: false, error: true }); })
-    }
-          
+        .catch(err => { obj.setState({ priceSquareMeter: [], salesByRegions: [], isLoadedSalesByRegions : false, isLoadedSquareMeter: false, error: true }); })
+        fetch('https://localhost:8443/property_value/sales_by_regions')
+          .then(res => res.json())
+          .then((data) => {
+            let value = [];
+            for (let i in data["hydra:member"]) {
+              let region = data["hydra:member"][i]["region"];
+              value[region] = data["hydra:member"][i]["sales"];
+
+            }
+            obj.setState({ ...this.state, salesByRegions: value, isLoadedSalesByRegions : true, error: false });
+          })
+          .catch(err => { obj.setState({...this.state, isLoadedSalesByRegions : false, error: true }); })
+      }
+
     render(){
       return (
         <div>
             <GraphTimeSeries priceSquareMeter={this.state.priceSquareMeter} isLoaded={this.state.isLoadedSquareMeter} error={this.state.error}/>
-            <SalesByRegions salesByRegions={this.state.salesByRegions} isLoaded={this.state.isLoadedSquareMeter} error={this.state.error}/>
+            <SalesByRegions salesByRegions={this.state.salesByRegions} isLoaded={this.state.isLoadedSalesByRegions} error={this.state.error}/>
         </div>
       )
     }
   }
-      
+
   export default App;
