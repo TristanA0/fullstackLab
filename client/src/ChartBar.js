f5ae5c6a51877c269d175a50c91efd85da6e96cd
TristanA0 <tristanlj@hotmail.fr>
2020-01-06 17:47:48 +0100

    adapt label

--- client/src/ChartBar.js
+++ client/src/ChartBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import './style.css';
 
 class ChartBar extends React.Component {
     componentDidMount() {
@@ -25,8 +26,10 @@ class ChartBar extends React.Component {
 
     draw() {
       let data = this.props.salesInterval;
+      console.log(data.length);
 
       d3.select("#graph-chartbar").html("");
+      d3.select("#tooltip-chartbar").html("");
 
       var margin = {top: 20, right: 20, bottom: 30, left: 40},
           width = 960 - margin.left - margin.right,
@@ -40,15 +43,30 @@ class ChartBar extends React.Component {
       var y = d3.scaleLinear()
                 .range([height, 0]);
 
+
+
+
+
+
+      var xAxis = d3.axisBottom()
+        .scale(x);
+
+      var yAxis = d3.axisLeft()
+        .scale(y);
+
       // append the svg object to the body of the page
       // append a 'group' element to 'svg'
       // moves the 'group' element to the top left margin
       var svg = d3.select("#graph-chartbar").append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+          .append("g")
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
+
+      const div = d3.select("#tooltip-chartbar").append("div")
+              .attr("class", "tooltip")
+              .style("opacity", 0);
 
 
       // format the data
@@ -57,28 +75,45 @@ class ChartBar extends React.Component {
       });
 
       // Scale the range of the data in the domains
-      x.domain(data.map(function(d) { return d.date; }));
+      //if(data.length <= 10 ) {
+        x.domain(data.map(function(d) { return d.date; }));
+      //}
       y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
       // append the rectangles for the bar chart
       svg.selectAll(".bar")
           .data(data)
-        .enter().append("rect")
+          .enter().append("rect")
           .attr("class", "bar")
           .attr("x", function(d) { return x(d.date); })
           .attr("width", x.bandwidth())
           .attr("y", function(d) { return y(d.value); })
           .attr("height", function(d) { return height - y(d.value); })
-          .style("fill", "#341EBF");
+          .style("fill", "#341EBF")
+          .on("mouseover", function(d) {
+              div.transition()
+                  .duration(200)
+                  .style("opacity", .9);
+              div.html("Date : " + d.date)
+                  .style("left", (d3.event.pageX + 10) + "px")
+                  .style("top", (d3.event.pageY - 50) + "px");
+          })
+          .on("mouseout", function(d) {
+              div.transition()
+                  .duration(500)
+                  .style("opacity", 0);
+          });
 
       // add the x Axis
-      svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
+      if(data.length <= 10 ) {
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+      }
 
       // add the y Axis
       svg.append("g")
-          .call(d3.axisLeft(y));
+          .call(yAxis);
 
 
 
@@ -87,6 +122,7 @@ class ChartBar extends React.Component {
     render(){
       return (
           <div>
+            <div id="tooltip-chartbar"> </div>
             <div id="graph-chartbar"><p>Loading...</p></div>
           </div>
       )
